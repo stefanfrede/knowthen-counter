a8ba0f6800153359f258acadb345aa91db86b18a
Stefan Frede <git@frede.io>
2018-09-05 18:20:10 +0200

    Switch .babelrc with babel.config.js

---
+++ babel.config.js
@@ -0,0 +1,12 @@
+module.exports = {
+  plugins: [
+    '@babel/plugin-syntax-dynamic-import',
+    [
+      '@babel/plugin-transform-runtime',
+      {
+        corejs: 2,
+      },
+    ],
+  ],
+  presets: [['@babel/preset-env', { modules: false }]],
+};
